c2896594e89cde268114eb388627dee9e86d9b5f
JESSIEWANTSLEARN <palaraojessie19@gmail.com>
2026-08-06 02:23:04 +0800

    PracticeAtmLogic

---
+++ CSP108ProgramingLanguages/src/LabExamATM/ATM.java
@@ -0,0 +1,78 @@
+package LabExamATM;
+import java.util.Scanner;
+
+public class ATM {
+	private static Scanner input = new Scanner(System.in);
+private float balanced;
+
+
+  public void deposit() {
+	  System.out.println("Enter Amount Deposit");
+	 float deposit =input.nextFloat();
+	 balanced +=   deposit; 
+  }
+  
+  
+  public void withDrawal() {
+	  System.out.println("Enter Amount Deposit");
+		 float withdraw =input.nextFloat();
+		 balanced -= withdraw;
+	  
+  }
+
+public void checkBalance() {
+	System.out.println("Balance"+balanced);
+	
+}
+
+
+public void menu () {
+	ATM  atm = new ATM();
+	boolean Loop = true;
+	while(Loop) {
+System.out.println("Menu");
+
+System.out.println("1:deposit");
+System.out.println("2:withDrawal");
+System.out.println("3:checkBalance");
+
+System.out.println("Choose the operation you want");
+	int choice =input.nextInt();
+	
+	
+	switch (choice){
+		
+		
+	case 1:
+		atm.deposit();
+ 	  break;
+	case 2:
+		atm.withDrawal();
+       break ;
+
+	case 3:
+		atm.checkBalance();
+   break;
+	case 4:
+		Loop = false;
+        break;
+        
+        default:
+        	System.out.println("Error please try again");
+	}
+
+	
+	}
+	
+	
+}
+	
+	
+	
+
+	
+	
+	
+	
+
+}
---
+++ CSP108ProgramingLanguages/src/LabExamATM/Main.java
@@ -0,0 +1,8 @@
+package LabExamATM;
+
+public class Main {
+	public static void main(String[] args) {
+		ATM atm = new ATM();
+		atm.menu();
+}
+}
